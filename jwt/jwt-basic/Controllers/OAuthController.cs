a3e5d754a3e2f724b735dfda07f6e1e2bb0d1371
bsaranga <bsaranga@gmail.com>
2022-06-09 08:14:14 +0530

    oauth client

--- jwt/jwt-basic/Controllers/OAuthController.cs
+++ jwt/jwt-basic/Controllers/OAuthController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace jwt_basic.Controllers
@@ -5,15 +6,20 @@ namespace jwt_basic.Controllers
     public class OAuthController : Controller
     {
         [HttpGet]
-        public IActionResult Authorize()
+        public IActionResult Authorize(string response_type, string client_id, string redirect_uri, string scope, string state)
         {
-            return View();
+            var query = new QueryBuilder();
+            query.Add("redirectUri", redirect_uri);
+            query.Add("state", state);
+
+            return View(model: query.ToString());
         }
 
         [HttpPost]
-        public IActionResult Authorize(string username)
+        public IActionResult Authorize(string username, string redirect_uri, string state)
         {
-            return View();
+            const string code = "65df65sd45s4a5sd45as4d5as21das5d";
+            return Redirect($"{redirect_uri}");
         }
 
         public IActionResult Token()
